c6514bd99dab350faba14b6d25bf5b534ee2462a
suuyyeon <127702506+suuyyeon@users.noreply.github.com>
2023-11-21 23:50:27 +0900

    Update ServicebasketDao.java

--- ServicebasketDao.java
+++ ServicebasketDao.java
@@ -1,5 +1,5 @@
+//장바구니 기능(추가, 삭제, 선택삭제) 중 선택삭제 부분만 남음
 //pid도 장바구니 DB에 넣어줘야하는지 고민하기
-//장바구니 기능(추가, 삭제, 선택삭제) 중 삭제 부분만 남음
 package dao;
 
 import java.sql.*;
@@ -10,13 +10,15 @@ import util.ConnectionPool;
 
 public class ServicebasketDao {
 	private static final String SELECT_PRODUCT_INFO_QUERY="SELECT pname, quantity, price FROM shoppingmall WHERE pid=?";
-	private static final String INSERT_BASKET_ITEM_QUERY="INSERT INTO basket (pname, quantity, price) VALUES (?,?,?)";
+	private static final String INSERT_BASKET_ITEM_QUERY="INSERT INTO addbaskettable (pname, quantity, price) VALUES (?,?,?)";
+	private static final String DELETE_ALL_ITEMS_QUERY="DELETE FROM addbaskettable";
 	
+	//상품저장
 	public boolean addToShoppingBasket(int pid)
 	{
 		try(Connection conn=ConnectionPool.get();
 				PreparedStatement selectstmt=conn.prepareStatement(SELECT_PRODUCT_INFO_QUERY);
-				PreparedStatement insertstmt=conn.prepareStatement(INSERT_BASKET_ITEM_QUERY);)
+				PreparedStatement insertstmt=conn.prepareStatement(INSERT_BASKET_ITEM_QUERY))
 		{
 			selectstmt.setInt(1, pid);
 			ResultSet rs=selectstmt.executeQuery();
@@ -35,6 +37,24 @@ public class ServicebasketDao {
 				
 				return rowsAffected>0;
 			}
+		}
+		catch(SQLException|NamingException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	//전체상품 삭제
+	public boolean deleteALLitems()
+	{
+		try(Connection conn=ConnectionPool.get();
+			PreparedStatement stmt=conn.prepareStatement(DELETE_ALL_ITEMS_QUERY))
+		{
+			int rowsAffected=stmt.executeUpdate();
+			
+			return rowsAffected>0;
 		}
 		catch(SQLException|NamingException e)
 		{
