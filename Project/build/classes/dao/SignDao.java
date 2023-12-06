536fedee2db2d3b96796b63e89bcf62b5b28e65d
108TowaKJ <150217811+108TowaKJ@users.noreply.github.com>
2023-12-06 22:28:33 +0900

    SignDao.java

--- Project/build/classes/dao/SignDao.java
+++ Project/build/classes/dao/SignDao.java
@@ -1,17 +1,21 @@
 package dao;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.naming.NamingException;
+
 import util.ConnectionPool;
 
 public class SignDao {
-	public boolean insert(String uid, String upass, String uname, String udad) 
+	private String spw;
+	public boolean insert(String uid, String upass, String uname, String udad, String utel) 
 			throws NamingException, SQLException{
 		Connection conn=null;
 		PreparedStatement stmt=null;
 		try {
-			String sql="insert into user(id,pw,name,daddress) values(?,?,?,?)";
+			String sql="insert into user values(?,?,?,?,?)";
 			conn=ConnectionPool.get();
 			stmt=conn.prepareStatement(sql);
 			
@@ -19,6 +23,7 @@ public class SignDao {
 			stmt.setString(2, upass);
 			stmt.setString(3, uname);
 			stmt.setString(4, udad);
+			stmt.setString(5, utel);
 			
 			int count=stmt.executeUpdate();
 			return (count>0)? true:false;
@@ -40,6 +45,27 @@ public class SignDao {
 			stmt.setString(1, uid);
 			rs=stmt.executeQuery();
 			return rs.next();
+		}finally {
+			if(rs!=null) rs.close();
+			if(stmt!=null) stmt.close();
+			if(conn!=null) conn.close();
+		}
+	}
+	public String select() 
+			throws NamingException, SQLException{
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			String sql="select * from user";
+			conn=ConnectionPool.get();
+			stmt=conn.prepareStatement(sql);
+			rs = stmt.executeQuery(sql);
+			String spw="";
+			while(rs.next()) {
+				spw=rs.getString("pw");
+			}
+			return spw;
 		}finally {
 			if(rs!=null) rs.close();
 			if(stmt!=null) stmt.close();
