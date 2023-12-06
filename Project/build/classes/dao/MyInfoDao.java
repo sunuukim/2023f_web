3b549781adafcddb9d6a428e32c81ef641fc1d45
108TowaKJ <150217811+108TowaKJ@users.noreply.github.com>
2023-12-06 22:30:03 +0900

    MyInfoDao.java

---
+++ Project/build/classes/dao/MyInfoDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.sql.*;
+import java.util.*;
+import javax.naming.NamingException;
+import util.ConnectionPool;
+
+public class MyInfoDao {
+	public String select() 
+			throws NamingException, SQLException{
+		Connection conn=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			String sql="select * from user";
+			conn=ConnectionPool.get();
+			stmt=conn.prepareStatement(sql);
+			rs = stmt.executeQuery(sql);
+			String str="";
+			while(rs.next()) {
+				str+=rs.getString("id");
+			}
+			return str;
+		}finally {
+			if(rs!=null) rs.close();
+			if(stmt!=null) stmt.close();
+			if(conn!=null) conn.close();
+		}
+	}
+}
